2da87a2ea60669bb78d8deb4603e7026abed3b24
rznegrr <negarrz1378@gmail.com>
2025-02-03 22:11:31 +0330

    ✨ feat: create product sidebar

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,14 +1,25 @@
 import ProductCard from "@/components/cards/ProductCard";
+import ProductSidebar from "@/components/product_sidebar/ProductsSideBar";
 import React from "react";
 
 export default async function Products() {
   return (
-    <div className="grid grid-cols-1 place-items-center gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+    <div className="grid grid-cols-1 lg:grid-cols-12 gap-4">
+      {/* Sidebar */}
+      <div className="hidden lg:block lg:col-span-3 place-items-center">
+        <ProductSidebar />
+      </div>
+
+      {/* Product Grid */}
+      <div className="lg:col-span-9 place-items-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-5 gap-10 xl:gap-5">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+      </div>
     </div>
   );
 }
