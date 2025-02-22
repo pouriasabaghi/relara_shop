498eee8debbab3c1c1e8265fad80d3319e7061bb
rznegrr <negarrz1378@gmail.com>
2025-02-22 16:53:03 +0330

    🧑‍💻 chore: add icon for panel left items

--- app/(user)/panel/layout.tsx
+++ app/(user)/panel/layout.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -7,15 +7,22 @@ import { ReactNode } from "react";
 import MainHeader from "@/layouts/MainHeader";
 import DesktopHeader from "@/layouts/DesktopHeader";
 import MainFooter from "@/layouts/MainFooter";
+import {
+  PiAddressBook,
+  PiCalendarHeart,
+  PiShoppingBagOpenDuotone,
+  PiPhoneCall,
+  PiUserCircle
+} from "react-icons/pi";
+import { RxDashboard } from "react-icons/rx";
 
 const menuItems = [
-  { name: "dashboard", href: "/panel" },
-  { name: "orders", href: "/panel/orders" },
-  { name: "addresses", href: "/panel/addresses" },
-  { name: "profile", href: "/panel/profile" },
-  { name: "wishlist", href: "/panel/wishlist" },
-  { name: "support", href: "/panel/support" },
-  { name: "settings", href: "/panel/settings" },
+  { name: "Dashboard", icon: <RxDashboard />, href: "/panel" },
+  { name: "Profile", icon: <PiUserCircle />, href: "/panel/profile" },
+  { name: "Orders", icon: <PiShoppingBagOpenDuotone />, href: "/panel/orders" },
+  { name: "Addresses", icon: <PiAddressBook />, href: "/panel/addresses" },
+  { name: "Wishlist", icon: <PiCalendarHeart />, href: "/panel/wishlist" },
+  { name: "Support", icon: <PiPhoneCall />, href: "/panel/support" },
 ];
 
 export default function PanelLayout({ children }: { children: ReactNode }) {
@@ -30,19 +37,20 @@ export default function PanelLayout({ children }: { children: ReactNode }) {
         <DesktopHeader />
       </div>
 
-      <div className="m-auto overflow-auto px-4 pb-20 pt-10 lg:pt-52 flex gap-4 h-screen">
+      <div className="m-auto flex h-screen gap-4 overflow-auto px-4 pb-20 pt-10 lg:pt-52">
         {/* Sidebar */}
-        <div className="w-64 border-l bg-white p-4 shadow-md rounded-tl-lg rounded-bl-lg">
+        <div className="w-64 rounded-bl-lg rounded-tl-lg border-l bg-white p-4 shadow-md">
           <nav className="space-y-2">
             {menuItems.map((item) => (
               <Link
                 key={item.href}
                 href={item.href}
                 className={cn(
-                  "block rounded-lg px-4 py-2 text-gray-600 hover:text-white hover:bg-customPrimary font-semibold",
+                  "flex items-center gap-2 rounded-lg px-4 py-2 font-semibold text-gray-600 hover:bg-customPrimary hover:text-white",
                   pathname === item.href && "bg-customPrimary text-white",
                 )}
               >
+                <span className="text-lg">{item.icon}</span>
                 {item.name}
               </Link>
             ))}
@@ -50,7 +58,9 @@ export default function PanelLayout({ children }: { children: ReactNode }) {
         </div>
 
         {/* Main Content */}
-        <main className="bg-white w-full p-5 rounded-tr-lg rounded-br-lg shadow-md">{children}</main>
+        <main className="w-full rounded-br-lg rounded-tr-lg bg-white p-5 shadow-md">
+          {children}
+        </main>
       </div>
 
       <div className="hidden lg:block">
