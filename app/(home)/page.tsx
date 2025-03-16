39365c6345a5936636e6740e87ed789ab8efe4f1
rznegrr <negarrz1378@gmail.com>
2025-03-16 21:18:52 +0330

    ✨feat: add dynamic route to product detail page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,20 +1,22 @@
+"use client";
+
 import ProductCard from "@/components/cards/ProductCard";
+import React from "react";
 
 export default function Home() {
-  return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-10 place-items-center">
-    <ProductCard/>
-    <ProductCard/>
-    <ProductCard/>
-    <ProductCard/>
-    <ProductCard/>
-    <ProductCard/>
-    <ProductCard/>
-    <ProductCard/>
-    <ProductCard/>
-    <ProductCard/>
-    <ProductCard/>
-    <ProductCard/>
-    <ProductCard/>
-    <ProductCard/>
-  </div>;
+  return (
+    <div>
+      <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-4 xl:gap-5 2xl:grid-cols-5 place-items-center">
+        <ProductCard name="Product Name" image="/images/image.png" price="$99.99" slug={1}/>
+        <ProductCard name="Product Name" image="/images/image.png" price="$99.99" slug={1}/>
+        <ProductCard name="Product Name" image="/images/image.png" price="$99.99" slug={1}/>
+        <ProductCard name="Product Name" image="/images/image.png" price="$99.99" slug={1}/>
+        <ProductCard name="Product Name" image="/images/image.png" price="$99.99" slug={1}/>
+        <ProductCard name="Product Name" image="/images/image.png" price="$99.99" slug={1}/>
+        <ProductCard name="Product Name" image="/images/image.png" price="$99.99" slug={1}/>
+        <ProductCard name="Product Name" image="/images/image.png" price="$99.99" slug={1}/>
+        <ProductCard name="Product Name" image="/images/image.png" price="$99.99" slug={1}/>
+      </div>
+    </div>
+  );
 }
