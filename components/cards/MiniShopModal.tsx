e88027a5076ad727660519bdb2e9f8d956bfab64
rznegrr <negarrz1378@gmail.com>
2025-04-28 21:55:30 +0330

    🧑‍💻 chore: rename variables and fix small bugs

--- components/cards/MiniShopModal.tsx
+++ components/cards/MiniShopModal.tsx
@@ -1,19 +1,21 @@
+import { useCart } from "@/context/CartContext";
 import ShopCard from "./ShopCard";
 
 export default function MiniShopCart() {
+  const {cartItems} = useCart()
   return (
     <div className="fixed right-10 top-20 z-50 rounded-md border border-gray-200 bg-white p-3 shadow-lg">
-      <div className="space-y-2 max-h-72 mb-2 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300">
-        <ShopCard />
-        <ShopCard />
-        <ShopCard />
-        <ShopCard />
+  {cartItems.length > 0 ? <div>
+        <div className="space-y-2 max-h-72 mb-2 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300">
+       { cartItems.map((item) => (
+          <ShopCard key={item.id} title={item.title} price={item.price} image={item.image} quantity={item.quantity} id={item.id}/>
+        ))}
       </div>
 
       <div className="border-t p-4">
         <div className="flex justify-between text-lg font-semibold">
           <span className="text-black">Total:</span>
-          <span className="text-black">$1199</span>
+          <span className="text-black">${cartItems.reduce((total, item) => total + parseFloat(item.price), 0).toFixed(2)}</span>
         </div>
         <button className="mt-4 w-full rounded-md bg-customPrimary px-4 py-2 text-sm font-bold text-white transition hover:bg-primaryDark">
           Checkout
@@ -22,6 +24,7 @@ export default function MiniShopCart() {
           View Cart
         </button>
       </div>
+  </div>: <p className="text-black p-3">Your shopping cart is empty.</p>}
     </div>
   );
 }
--- components/cards/ShopCard.tsx
+++ components/cards/ShopCard.tsx
@@ -1,14 +1,39 @@
+import { useCart } from "@/context/CartContext";
 import Image from "next/image";
 import React from "react";
 import { FaMinus, FaPlus, FaTrash } from "react-icons/fa6";
 
-export default function ShopCard() {
+type ShopCard= {
+  id: string,
+  title: string,
+  price: string,
+  quantity: number
+  image: string  
+}
+
+export default function ShopCard({title,price,image,id,quantity}:ShopCard) {
+  const {addToCart,removeFromCart} = useCart()
+
+  const addToCartHandler = () => {
+   addToCart({
+          id: id,
+          image: image,
+          title: title,
+          price: price,
+          quantity: quantity,
+        })
+  }
+
+  const removeFromCartHandler = () => {
+    removeFromCart(id)
+  }
+
   return (
     <div className="relative flex w-full max-w-xl items-center gap-4 overflow-hidden rounded-lg border border-gray-200 bg-white p-4 shadow-md transition-all hover:shadow-lg md:max-w-2xl">
       {/* product image */}
       <div className="relative h-28 w-28 overflow-hidden rounded-lg border border-gray-200">
         <Image
-          src="/images/productImage.png"
+          src={image}
           alt="Product image"
           className="object-cover"
           layout="fill"
@@ -19,12 +44,11 @@ export default function ShopCard() {
       <div className="flex flex-1 flex-col justify-between">
         {/* product name */}
         <p className="text-sm md:text-lg font-semibold text-customPrimary line-clamp-1 mt-2 md:mt-0">
-          iPhone 14 Pro Max
+          {title}
         </p>
 
         {/* product color */}
         <div className="flex items-center gap-2">
-          <div className="h-5 w-5 rounded-full border border-gray-300 bg-pink-300"></div>
           <p className="text-sm text-gray-600">Pink</p>
         </div>
 
@@ -36,17 +60,17 @@ export default function ShopCard() {
         <div className="mt-3 flex items-center justify-between">
           {/* price */}
           <div className="flex flex-col">
-            <del className="text-xs md:text-sm text-gray-400 pl-5 md:pl-10">$1500.2</del>
-            <p className="text-md md:text-lg font-semibold text-gray-900">$1250.2</p>
+            {/* <del className="text-xs md:text-sm text-gray-400 pl-5 md:pl-10">$1500.2</del> */}
+            <p className="text-md md:text-lg font-semibold text-gray-900">${price}</p>
           </div>
 
           {/* increment and decrement buttons */}
           <div className="flex items-center gap-2 md:gap-4 rounded-md border border-gray-300 bg-gray-100 px-2 py-1 ml-3">
-            <button className="text-gray-600 transition hover:text-gray-900 text-sm md:text-base">
+            <button className="text-gray-600 transition hover:text-gray-900 text-sm md:text-base" onClick={removeFromCartHandler}>
               <FaMinus />
             </button>
             <span className="text-sm font-semibold text-gray-800">1</span>
-            <button className="text-gray-600 transition hover:text-gray-900 text-sm md:text-base">
+            <button className="text-gray-600 transition hover:text-gray-900 text-sm md:text-base" onClick={addToCartHandler}>
               <FaPlus />
             </button>
           </div>
